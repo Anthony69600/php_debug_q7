e6308a70aeb3f3dc61f1235847bfcd3ce48347c5
Anthony VERCHERE <76996157+Anthony69600@users.noreply.github.com>
2021-09-28 21:45:44 +0200

    first_commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ],
+    "Anthony" => [
+        "city" => "Lyon",
+        "weapon" => "Katana"
+    ]
+
+];
+
+
+
+// We write code here
+
+var_dump($characters);
